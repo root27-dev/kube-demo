41a16d4a090724f7d00a57711a05e4d208c67ef0
root27 <oguzhaneee@gmail.com>
2024-12-10 15:09:25 +0300

    change index page

--- main.go
+++ main.go
@@ -7,11 +7,22 @@ import (
 
 func main() {
 
-	http.HandleFunc("/demo", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
 		w.WriteHeader(http.StatusOK)
 
-		w.Write([]byte("Hello Kubernetes"))
+		index := []byte(`<!DOCTYPE html>
+				<html>
+				<head>
+					<title>My Kubernetes App</title>
+				</head>
+				<body>
+					<p> Request from: ` + r.RemoteAddr + `</p>
+					<p> Hostname: ` + r.Host + `</p>
+				</body>
+				</html>`)
+
+		w.Write(index)
 	})
 
 	fmt.Println("Server started at port 8080")
